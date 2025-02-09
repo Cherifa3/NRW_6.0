65921062b0f35b4d567472afa09b8ac936419225
Youssef-Guiga <youssefguiga@gmail.com>
2025-02-09 17:54:44 +0100

    navbar+cards components

--- NRW/src/app/app.module.ts
+++ NRW/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
