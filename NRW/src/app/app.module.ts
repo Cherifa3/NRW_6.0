1eff94431dbe95c6eb666cd77504e995109446bd
skan45 <gritliskander42@gmail.com>
2025-02-10 01:55:17 +0100

    added program-list component

--- NRW/src/app/app.module.ts
+++ NRW/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProgramListComponent } from './program-list/program-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    ProgramListComponent
   ],
   imports: [
     BrowserModule,
